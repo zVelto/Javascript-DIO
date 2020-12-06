6a070ac55152b029256741c6a65a0aa27ebfde9e
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-12-06 20:25:00 -0300

    Introdução ao javascript

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome = "Welton Carvalho"
+var n1 = 5
+var n2 = 3
+var frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos")
+//alert(idade + idade2)
+console.log(nome)
+console.log(n1 * n2)
+console.log(frase.toLowerCase())
+//alert(frase.replace("Japão", "Brasil"))
