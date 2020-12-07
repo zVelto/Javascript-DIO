1e5774c035cc392eec2509e6d531594644600999
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-12-06 21:07:51 -0300

    Condicionais, laços de repetição e Date

--- js/main.js
+++ js/main.js
@@ -1,6 +1,36 @@
+
+var d = new Date
+alert(d.getMinutes())
+
+/*
+var count
+for(count = 0; count < 5; count++){
+    console.log(count)
+}
+*/
+
+/*
+var count = 0
+while(count <= 5){
+    console.log(count)
+    count++
+}
+*/
+
+/*
+var idade = prompt("Qual a sua idade?")
+if(idade >= 18) {
+    alert("Maior de idade")
+}else{
+    alert("Menor de idade")
+}
+*/
+
+/*
 var frutas = [{nome:"maçã", cor:"vermelha"},{nome:"uva", cor:"roxa"}]
 console.log(frutas)
 alert(frutas[1].cor)
+*/
 
 /*
 var lista = ["maçã", "pêra", "laranja"]
